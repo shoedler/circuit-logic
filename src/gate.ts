7f8aba0ee1eb5961eee0fa42bcddfd934a4076a3
Simon Schödler <simon.schoedler@nexplore.ch>
2023-09-14 17:20:48 +0200

    Adds lamp gate, refactors

--- src/gate.ts
+++ src/gate.ts
@@ -1,9 +1,36 @@
 interface IDisposable {
-    dispose(): void;
+  dispose(): void;
 }
 
 interface IRedrawable {
-    redraw(): void;
+  redraw(): void;
+}
+
+/**
+ * Construction parameters for a gate.
+ * @param bounds The parent html element to which the gate will be appended.
+ * @param name The name of the gate. (Displayed in the gate's label)
+ * @param inputs The names of the gate's inputs, will be displayed on the left side of the gate.
+ * @param outputs The names of the gate's outputs, will be displayed on the right side of the gate.
+ * @param color The color of the gate.
+ * @param init A function that will be called when the gate is created. Can be used to add additional elements to the gate. It's context is also available in the gate's logic function, @see GateParams.logic
+ * @param logic The logic function of the gate. Will be called when the gate is run. The inputs and outputs are passed as boolean arrays. Also receives the gate itself as a third parameter, which can be used to manipulate the gate's DOM Element.
+ */
+export interface GateParams {
+  bounds: HTMLDivElement;
+  name: string;
+  inputs?: string[];
+  outputs?: string[];
+  color?:
+    | "red"
+    | "green"
+    | "yellow"
+    | "blue"
+    | "turquoise"
+    | "darkgreen"
+    | "gray";
+  init?: (self: Gate) => void;
+  logic: (inputs: boolean[], outputs: boolean[], self?: Gate) => void;
 }
 
 export class Gate extends HTMLDivElement implements IDisposable, IRedrawable {
@@ -11,39 +38,49 @@ export class Gate extends HTMLDivElement implements IDisposable, IRedrawable {
   private readonly _inputs: GateConnectorCollection;
   private readonly _name: HTMLParagraphElement;
   private readonly _outputs: GateConnectorCollection;
-  private readonly _logic: (inputs: boolean[], outputs: boolean[], self?: Gate) => void
+  private readonly _logic: (
+    inputs: boolean[],
+    outputs: boolean[],
+    self?: Gate
+  ) => void;
 
-  public get name(): string { return this._name.innerText;  }
-  private set name(v: string) { this._name.innerText = v; }
+  public get name(): string {
+    return this._name.innerText;
+  }
+  private set name(v: string) {
+    this._name.innerText = v;
+  }
 
-  constructor(params: { 
-    bounds: HTMLDivElement, 
-    name: string, 
-    inputs?: string[], 
-    outputs?: string[], 
-    color?: "red" | "green" | "blue" | "turquoise" | "darkgreen" | "gray",
-    logic: (inputs: boolean[], outputs: boolean[], self?: Gate) => void
-  }) {
+  constructor(params: GateParams) {
     super();
+    const logicContext: any = {};
 
-    this.classList.add('gate');
+    this.classList.add("gate");
     this.id = `gate-${Gate._gateCount++}`;
-    this._logic = params.logic;
+    this._logic = params.logic.bind(logicContext);
+
+    if (params.init) {
+      params.init.bind(logicContext)(this);
+    }
 
     if (params.color) {
       this.style.backgroundColor = `var(--color-${params.color})`;
     }
 
     if (params.inputs) {
-      this._inputs = new GateConnectorCollection(this, 'inputs', params.inputs);
+      this._inputs = new GateConnectorCollection(this, "inputs", params.inputs);
     }
- 
-    this._name = document.createElement('p');
-    this._name.classList.add('gate-label');
+
+    this._name = document.createElement("p");
+    this._name.classList.add("gate-label");
     this.appendChild(this._name);
-    
+
     if (params.outputs) {
-      this._outputs = new GateConnectorCollection(this, 'outputs', params.outputs);
+      this._outputs = new GateConnectorCollection(
+        this,
+        "outputs",
+        params.outputs
+      );
     }
 
     this.name = params.name;
@@ -52,28 +89,31 @@ export class Gate extends HTMLDivElement implements IDisposable, IRedrawable {
   }
 
   public run = (): void => {
-    const inputs = this._inputs.toBoolArray()
-    const outputs = this._outputs.toBoolArray()
-    this._logic(inputs, outputs, this)
-    this._outputs.fromBoolArray(outputs)
-  }
+    const inputs = this._inputs.toBoolArray();
+    const outputs = this._outputs.toBoolArray();
+    this._logic(inputs, outputs, this);
+    this._outputs.fromBoolArray(outputs);
+  };
 
   public redraw(): void {
-      this._inputs.redraw();
-      this._outputs.redraw();
+    this._inputs.redraw();
+    this._outputs.redraw();
   }
 
   public dispose = () => {
     this._inputs.dispose();
     this._outputs.dispose();
     this.remove();
-  }
+  };
 }
 
-export class GateConnectorCollection extends HTMLDivElement implements IDisposable, IRedrawable {
+export class GateConnectorCollection
+  extends HTMLDivElement
+  implements IDisposable, IRedrawable
+{
   private readonly _connectors: GateConnector[] = [];
-  public readonly type: 'inputs' | 'outputs';
-  constructor(owner: Gate, type: 'inputs' | 'outputs', connectors: string[]) {
+  public readonly type: "inputs" | "outputs";
+  constructor(owner: Gate, type: "inputs" | "outputs", connectors: string[]) {
     super();
 
     this.classList.add(`gate-${type}`);
@@ -85,88 +125,100 @@ export class GateConnectorCollection extends HTMLDivElement implements IDisposab
 
   public toBoolArray = (): boolean[] => this._connectors.map(c => c.isActive);
   public fromBoolArray = (values: boolean[]): void => {
-    if (this.type === 'inputs') 
-      throw new Error('Cannot set input values on an output collection');
+    if (this.type === "inputs")
+      throw new Error("Cannot set input values on an output collection");
     this._connectors.forEach((c, i) => c.toggleActive(values[i]));
-  }
+  };
 
   public redraw = (): void => {
     this._connectors.forEach(c => c.redraw());
-  }
+  };
 
   public dispose = (): void => {
     this._connectors.forEach(c => c.dispose());
     this.remove();
-  }
+  };
 }
 
-export class GateConnector extends HTMLDivElement implements IDisposable, IRedrawable {
+export class GateConnector
+  extends HTMLDivElement
+  implements IDisposable, IRedrawable
+{
   private readonly _name: string;
   private readonly _edges: GateEdge[] = [];
-  public get isActive(): boolean { return this._edges.some(edge => edge.isActive); }
-  public readonly type: () => 'inputs' | 'outputs';
+  public get isActive(): boolean {
+    return this._edges.some(edge => edge.isActive);
+  }
+  public readonly type: () => "inputs" | "outputs";
   constructor(parent: GateConnectorCollection, name: string) {
     super();
 
-    this.classList.add('gate-connector');
+    this.classList.add("gate-connector");
     parent.appendChild(this);
-    this.type = function () { return parent.type };
+    this.type = function () {
+      return parent.type;
+    };
 
     this._name = name;
   }
 
-  public toggleIllegal = (illegal: boolean): void => illegal ? this.classList.add('illegal') : this.classList.remove('illegal');
+  public toggleIllegal = (illegal: boolean): void =>
+    illegal ? this.classList.add("illegal") : this.classList.remove("illegal");
   public toggleActive = (active: boolean): void => {
-    if (this.type() === 'inputs') 
-      throw new Error('Cannot toggle active state of input connector');
+    if (this.type() === "inputs")
+      throw new Error("Cannot toggle active state of input connector");
     this._edges.forEach(e => e.toggleActive(active));
-  }
+  };
 
   public newEdge = (parent: SVGElement): GateEdge => {
     const edge = new GateEdge({ parent, start: this });
     this._edges.push(edge);
     return edge;
-  }
+  };
 
   public endEdge = (end: GateEdge): void => {
     end.end = this;
     this._edges.push(end);
-  }
+  };
 
   public removeEdge = (edge: GateEdge): void => {
     const index = this._edges.indexOf(edge);
     if (index > -1) {
       this._edges.splice(index, 1);
     }
-  }
+  };
 
   public redraw = (): void => {
     this._edges.forEach(e => e.redraw());
-  }
+  };
 
   public dispose = (): void => {
     this._edges.forEach(e => e.dispose());
     this.remove();
-  }
+  };
 }
 
 export class GateEdge implements IDisposable, IRedrawable {
-  public readonly path: SVGPathElement
+  public readonly path: SVGPathElement;
   public readonly start: GateConnector;
-  private readonly _svg: Omit<SVGElement, 'remove'>;
-  public get isActive(): boolean { return this.path.classList.contains('active'); }
-  private _end : GateConnector;
-  public get end() : GateConnector { return this._end; }
-  public set end(v : GateConnector) { 
-    this._end = v; 
-    this.path.classList.remove('drawing');
+  private readonly _svg: Omit<SVGElement, "remove">;
+  public get isActive(): boolean {
+    return this.path.classList.contains("active");
+  }
+  private _end: GateConnector;
+  public get end(): GateConnector {
+    return this._end;
+  }
+  public set end(v: GateConnector) {
+    this._end = v;
+    this.path.classList.remove("drawing");
   }
 
-  constructor(params: { parent: SVGElement, start: GateConnector }) {
+  constructor(params: { parent: SVGElement; start: GateConnector }) {
     this.start = params.start;
-    this.path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    this.path.classList.add('drawing');
-    this.path.addEventListener('dblclick', _ => {
+    this.path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    this.path.classList.add("drawing");
+    this.path.addEventListener("dblclick", _ => {
       if (this.end) this.end.removeEdge(this);
       if (this.start) this.start.removeEdge(this);
       this.dispose();
@@ -175,11 +227,20 @@ export class GateEdge implements IDisposable, IRedrawable {
     this._svg.appendChild(this.path);
   }
 
-  public toggleIllegal = (illegal: boolean): void => illegal ? this.path.classList.add('illegal') : this.path.classList.remove('illegal');
-  public toggleLegal = (legal: boolean): void => legal ? this.path.classList.add('legal') : this.path.classList.remove('legal');
-  public toggleActive = (active: boolean): void => active ? this.path.classList.add('active') : this.path.classList.remove('active');
+  public toggleIllegal = (illegal: boolean): void =>
+    illegal
+      ? this.path.classList.add("illegal")
+      : this.path.classList.remove("illegal");
+  public toggleLegal = (legal: boolean): void =>
+    legal
+      ? this.path.classList.add("legal")
+      : this.path.classList.remove("legal");
+  public toggleActive = (active: boolean): void =>
+    active
+      ? this.path.classList.add("active")
+      : this.path.classList.remove("active");
 
-  public draw = (pos?: { x: number, y: number }): void => {
+  public draw = (pos?: { x: number; y: number }): void => {
     const offset = this._svg.getBoundingClientRect();
     const startOffset = this.start.getBoundingClientRect();
 
@@ -187,59 +248,73 @@ export class GateEdge implements IDisposable, IRedrawable {
       left: startOffset.left - offset.left,
       top: startOffset.top - offset.top,
       width: startOffset.width / 2,
-      height: startOffset.height / 2
-    }
+      height: startOffset.height / 2,
+    };
 
-    const end = pos ? {
-      left: pos.x - offset.left,
-      top: pos.y - offset.top,
-      width: 0,
-      height: 0
-    } : this.end ? {
-      left: this.end.getBoundingClientRect().left - offset.left,
-      top: this.end.getBoundingClientRect().top - offset.top,
-      width: this.end.getBoundingClientRect().width / 2,
-      height: this.end.getBoundingClientRect().height / 2
-    } : null;
+    const end = pos
+      ? {
+          left: pos.x - offset.left,
+          top: pos.y - offset.top,
+          width: 0,
+          height: 0,
+        }
+      : this.end
+      ? {
+          left: this.end.getBoundingClientRect().left - offset.left,
+          top: this.end.getBoundingClientRect().top - offset.top,
+          width: this.end.getBoundingClientRect().width / 2,
+          height: this.end.getBoundingClientRect().height / 2,
+        }
+      : null;
 
     if (!end) {
-      throw new Error('Cannot draw edge without end or position');
+      throw new Error("Cannot draw edge without end or position");
     }
 
     // Draw bezier. Start always has an angle of 0, end always has an angle of 180.
     // Additional control points are calculated based on the distance between the two points.
     // We want to make sure that there are always control points to the right of the start point, even if the end point is to the left.
-    // We also want to take the vertical distance between the two points into account, so that the curve doesn't 
+    // We also want to take the vertical distance between the two points into account, so that the curve doesn't
     // have too sharp of a turn if the end point is above or below the start point.
-    const distance = Math.sqrt(Math.pow(end.left - start.left, 2) + Math.pow(end.top - start.top, 2));
+    const distance = Math.sqrt(
+      Math.pow(end.left - start.left, 2) + Math.pow(end.top - start.top, 2)
+    );
     const controlPointDistance = Math.max(distance / 2, 100);
 
     const startControlPoint = {
       x: start.left + start.width + controlPointDistance,
-      y: start.top + start.height
-    }
+      y: start.top + start.height,
+    };
     const endControlPoint = {
       x: end.left - end.width - controlPointDistance,
-      y: end.top + end.height
-    }
+      y: end.top + end.height,
+    };
     const midControlPoint = {
       x: (start.left + start.width + end.left + end.width) / 2,
-      y: (start.top + start.height + end.top + end.height) / 2
-    }
+      y: (start.top + start.height + end.top + end.height) / 2,
+    };
 
-    const useControlPoints = Math.abs(start.top - end.top) > Math.abs(start.left - end.left);
+    const useControlPoints =
+      Math.abs(start.top - end.top) > Math.abs(start.left - end.left);
 
-    const path = useControlPoints ? 
-      `M ${start.left + start.width} ${start.top + start.height} C ${startControlPoint.x} ${startControlPoint.y} ${endControlPoint.x} ${endControlPoint.y} ${end.left - end.width} ${end.top + end.height}` :
-      `M ${start.left + start.width} ${start.top + start.height} C ${midControlPoint.x} ${start.top + start.height}, ${midControlPoint.x} ${end.top + end.height}, ${end.left + end.width} ${end.top + end.height}`;
+    const path = useControlPoints
+      ? `M ${start.left + start.width} ${start.top + start.height} C ${
+          startControlPoint.x
+        } ${startControlPoint.y} ${endControlPoint.x} ${endControlPoint.y} ${
+          end.left - end.width
+        } ${end.top + end.height}`
+      : `M ${start.left + start.width} ${start.top + start.height} C ${
+          midControlPoint.x
+        } ${start.top + start.height}, ${midControlPoint.x} ${
+          end.top + end.height
+        }, ${end.left + end.width} ${end.top + end.height}`;
 
-    this.path.setAttribute('d', path);
-  }
-
+    this.path.setAttribute("d", path);
+  };
 
   public redraw = (): void => {
-    this.draw()
-  }
+    this.draw();
+  };
 
   public dispose = (): void => {
     this.path.remove();
@@ -249,8 +324,7 @@ export class GateEdge implements IDisposable, IRedrawable {
     }
     if (this.start) {
       this.start.removeEdge(this);
-      this.start
+      this.start;
     }
-  }
+  };
 }
-
--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,227 @@
 import { Gate, GateConnector, GateConnectorCollection } from "./gate";
-import { EdgeDragHandler, GateDragHandler, IDragEventHandler } from "./dragEventHandler";
+import {
+  EdgeDragHandler,
+  GateDragHandler,
+  IDragEventHandler,
+} from "./dragEventHandler";
+
+const STATE = {
+  probeGateMarker: ["🔴", "🟠", "🟡", "🟢", "🔵", "🟣", "🟤"],
+  probeGateIndex: 0,
+};
 
 (() => {
-  document.addEventListener('DOMContentLoaded', _ => {
-    const gates: Gate[] = [ ];
+  document.addEventListener("DOMContentLoaded", _ => {
+    const gates: Gate[] = [];
 
-    const gatesContainer  = document.querySelector('.gates') as HTMLDivElement;
-    const edgesContainer  = document.querySelector('.edges') as HTMLDivElement;
-    const buttonsContainer = document.querySelector('.buttons') as HTMLDivElement;
+    const gatesContainer = document.querySelector(".gates") as HTMLDivElement;
+    const edgesContainer = document.querySelector(".edges") as HTMLDivElement;
+    const simSpeedometer = document.querySelector(
+      ".simulation-speedometer"
+    ) as HTMLParagraphElement;
+    const buttonsContainer = document.querySelector(
+      ".buttons"
+    ) as HTMLDivElement;
 
-    customElements.define('cl-gate', Gate, { extends: 'div' });
-    customElements.define('cl-gate-connector', GateConnector, { extends: 'div' });
-    customElements.define('cl-gate-connector-collection', GateConnectorCollection, { extends: 'div' });
+    // Register custom elements
+    customElements.define("cl-gate", Gate, { extends: "div" });
+    customElements.define("cl-gate-connector", GateConnector, {
+      extends: "div",
+    });
+    customElements.define(
+      "cl-gate-connector-collection",
+      GateConnectorCollection,
+      { extends: "div" }
+    );
 
-    const gateBuilders: { [key: string]: () => Gate} = {
-      "And": () => new Gate({
-        bounds: gatesContainer,
-        name: 'AND',
-        inputs: ['A', 'B'],
-        outputs: ['C'],
-        logic: (ins, outs) => {
-            outs[0] = ins[0] && ins[1];
-        }
-      }),
-      "Or": () => new Gate({
-        bounds: gatesContainer,
-        name: 'OR',
-        inputs: ['A', 'B'],
-        outputs: ['C'],
-        color: 'turquoise',
-        logic: (ins, outs) => {
-            outs[0] = ins[0] || ins[1];
-        }
-      }),
-      "True": () => new Gate({
-          bounds: gatesContainer,
-          name: '1',
-          inputs: [],
-          outputs: ['C'],
-          color: 'green',
-          logic: (ins, outs) => {
-              outs[0] = true;
-          }
-      }),
-      "2Hz Clock": () => new Gate({
-        bounds: gatesContainer,
-        name: '2Hz',
-        inputs: [],
-        outputs: ['C'],
-        color: 'darkgreen',
-        logic: (ins, outs) => {
-            outs[0] = (new Date().getMilliseconds() % 500) < 250;
-        }
-      }),
-      "1Hz Clock": () => new Gate({
-        bounds: gatesContainer,
-        name: '1Hz',
-        inputs: [],
-        outputs: ['C'],
-        color: 'darkgreen',
-        logic: (ins, outs) => {
-            outs[0] = (new Date().getMilliseconds() % 1000) < 500;
-        }
-      }),
-      "Probe": () => new Gate({
-        bounds: gatesContainer,
-        name: 'Probe',
-        inputs: ['A'],
-        outputs: ['C'],
-        color: 'gray',
-        logic: function(ins, outs, self) {
-          if (this.lastOn === undefined && this.lastOff === undefined) {
-            this.lastOn = null
-            this.lastOff = null
-          }
-          
-          if (ins[0] && this.lastOn === null) {
-            this.lastOn = Date.now()
-          }
-          else if (!ins[0] && this.lastOn !== null) {
-            console.log(`Probe on for ${Date.now() - this.lastOn}ms`)
-            this.lastOn = null
-          }
-          
-          if (!ins[0] && this.lastOff === null) {
-            this.lastOff = Date.now()
-          }
-          else if (ins[0] && this.lastOff !== null) {
-            console.log(`Probe off for ${Date.now() - this.lastOff}ms`)
-            this.lastOff = null
-          }
-
-          outs[0] = ins[0];
-        }
-      }),
-      "Not": () => new Gate({
-          bounds: gatesContainer,
-          name: 'NOT',
-          inputs: ['A'],
-          outputs: ['C'],
-          color: 'red',
-          logic: (ins, outs) => {
-              outs[0] = !ins[0]
-          }
-      }),
-      "Switch": () => new Gate({
-          bounds: gatesContainer,
-          name: "Switch",
-          inputs: [],
-          outputs: ['C'],
-          color: 'gray',
-          logic: function(_, outs, self) {
-            if (this.clicked === undefined) {
-              this.clicked = false;
-              self.addEventListener('click', _ => this.clicked = !this.clicked);
-            }
-            else {
-              outs[0] = this.clicked ?? false;
-            }
-          }
-      }),
-    }
+    // Create gate builder buttons
+    const gateBuilders = getGateBuilders(gatesContainer);
 
     Object.entries(gateBuilders).forEach(([name, builder]) => {
-      const button = document.createElement('button');
-      button.textContent = '➕ ' + name;
-      button.addEventListener('click', _ => gates.push(builder()));
+      const button = document.createElement("button");
+      button.textContent = "➕ " + name;
+      button.addEventListener("click", _ => gates.push(builder()));
       buttonsContainer.appendChild(button);
     });
 
+    // Start simulation
+    let simulationTimer = performance.now();
     setInterval(() => {
+      const simulationTime = performance.now() - simulationTimer;
+      const simulationTicks = Math.round(1000 / simulationTime);
+      simSpeedometer.textContent = `${simulationTicks} ticks/s`;
+
       gates.forEach(g => g.run());
+
+      simulationTimer = performance.now();
     }, 1000 / 60);
 
-
+    // Setup drag handler
     let currentDragHandler: IDragEventHandler = null;
 
-    document.addEventListener('mousedown', (e: MouseEvent) => {
+    document.addEventListener("mousedown", (e: MouseEvent) => {
       const element = document.elementFromPoint(e.clientX, e.clientY);
-      
+
       if (element instanceof GateConnector) {
         currentDragHandler = new EdgeDragHandler();
-      }
-      else if (element instanceof Gate || element.classList.contains('gate-label')) {
+      } else if (
+        element instanceof Gate ||
+        element.classList.contains("gate-label")
+      ) {
         currentDragHandler = new GateDragHandler();
-      }
-      else
-        return;
-    
+      } else return;
+
       currentDragHandler.onStart(e);
     });
-    document.addEventListener('mousemove', (e: MouseEvent) => {
+    document.addEventListener("mousemove", (e: MouseEvent) => {
       if (currentDragHandler) {
         currentDragHandler.onDrag(e);
       }
     });
-    document.addEventListener('mouseup', (e: MouseEvent) => {
+    document.addEventListener("mouseup", (e: MouseEvent) => {
       if (currentDragHandler) {
         currentDragHandler.onDrop(e);
         currentDragHandler = null;
       }
     });
 
-    EdgeDragHandler.attach({ attachee: edgesContainer })
+    EdgeDragHandler.attach({ attachee: edgesContainer });
   });
-})()
+})();
+
+const getGateBuilders = (
+  container: HTMLDivElement
+): { [key: string]: () => Gate } => {
+  return {
+    And: () =>
+      new Gate({
+        bounds: container,
+        name: "AND",
+        inputs: ["A", "B"],
+        outputs: ["C"],
+        logic: (ins, outs) => {
+          outs[0] = ins[0] && ins[1];
+        },
+      }),
+    Or: () =>
+      new Gate({
+        bounds: container,
+        name: "OR",
+        inputs: ["A", "B"],
+        outputs: ["C"],
+        color: "turquoise",
+        logic: (ins, outs) => {
+          outs[0] = ins[0] || ins[1];
+        },
+      }),
+    True: () =>
+      new Gate({
+        bounds: container,
+        name: "1",
+        inputs: [],
+        outputs: ["C"],
+        color: "green",
+        logic: (ins, outs) => {
+          outs[0] = true;
+        },
+      }),
+    "2Hz Clock": () =>
+      new Gate({
+        bounds: container,
+        name: "2Hz",
+        inputs: [],
+        outputs: ["C"],
+        color: "darkgreen",
+        logic: (ins, outs) => {
+          outs[0] = new Date().getMilliseconds() % 500 < 250;
+        },
+      }),
+    "1Hz Clock": () =>
+      new Gate({
+        bounds: container,
+        name: "1Hz",
+        inputs: [],
+        outputs: ["C"],
+        color: "darkgreen",
+        logic: (ins, outs) => {
+          outs[0] = new Date().getMilliseconds() % 1000 < 500;
+        },
+      }),
+    Probe: () =>
+      new Gate({
+        bounds: container,
+        name:
+          STATE.probeGateMarker[
+            STATE.probeGateIndex++ % STATE.probeGateMarker.length
+          ] + "Probe",
+        inputs: ["A"],
+        outputs: ["C"],
+        color: "gray",
+        init: function (self) {
+          this.lastOn = null;
+          this.lastOff = null;
+        },
+        logic: function (ins, outs, self) {
+          if (ins[0] && this.lastOn === null) {
+            this.lastOn = Date.now();
+          } else if (!ins[0] && this.lastOn !== null) {
+            console.log(`${self.name} on for  ${Date.now() - this.lastOn}ms`);
+            this.lastOn = null;
+          }
+
+          if (!ins[0] && this.lastOff === null) {
+            this.lastOff = Date.now();
+          } else if (ins[0] && this.lastOff !== null) {
+            console.log(`${self.name} off for ${Date.now() - this.lastOff}ms`);
+            this.lastOff = null;
+          }
+
+          outs[0] = ins[0];
+        },
+      }),
+    Not: () =>
+      new Gate({
+        bounds: container,
+        name: "NOT",
+        inputs: ["A"],
+        outputs: ["C"],
+        color: "red",
+        logic: (ins, outs) => {
+          outs[0] = !ins[0];
+        },
+      }),
+    Switch: () =>
+      new Gate({
+        bounds: container,
+        name: "Switch",
+        inputs: [],
+        outputs: ["C"],
+        color: "gray",
+        init: function (self) {
+          this.clicked = false;
+          self.addEventListener("click", _ => (this.clicked = !this.clicked));
+          self.classList.add("switch");
+        },
+        logic: function (_, outs, self) {
+          outs[0] = this.clicked;
+        },
+      }),
+    Lamp: () =>
+      new Gate({
+        bounds: container,
+        name: "💡",
+        inputs: ["A"],
+        outputs: [],
+        color: "gray",
+        init: function (self) {
+          this.on = false;
+        },
+        logic: function (ins, _, self) {
+          if (ins[0] !== this.on) {
+            this.on = ins[0];
+            self.classList.toggle("lamp-on", this.on);
+          }
+        },
+      }),
+  };
+};
